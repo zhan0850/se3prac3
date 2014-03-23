2c4c8e0bf442ee7f6fea5ae07d58b632ce1d494c
gardners <paul@servalproject.org>
2014-03-23 13:24:01 +1030

    move function prototypes to 2048.h to squash compile warnings.

--- 2048.h
+++ 2048.h
@@ -14,3 +14,6 @@ int tilt_board_down(int size,int **board);
 int **board_create(int size);
 int board_display(int size,int **board);
 int board_spawn_tile(int size,int **board);
+int combine_tiles(int length,int *line,int offset);
+int tilt_through_empty_space(int length, int *line);
+int tilt_line_left(int length,int *line);
