426af9eb1394840d0fc72af1da3c453bc1cf4e91
gardners <paul@servalproject.org>
2014-03-23 12:32:16 +1030

    create board, and display board after each turn.

--- main.c
+++ main.c
@@ -9,7 +9,15 @@ int main(int argc,char **argv)
   // Randomise random unmber generator initial state.
   srandom(time(0));
   
+  int board_size=4;
+  int **board=board_create(board_size);
+  if (!board) {
+    printf("failed to create board.\n");
+    exit(0);
+  }
+
   while(1) {
+    board_display(board_size,board);
     printf("Next move? "); fflush(stdout);
     int action=read_input();
     switch(action) {
