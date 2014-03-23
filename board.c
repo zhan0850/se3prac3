c8cdeaf927e6e7d2976c663f7e9f27f6a764c381
gardners <paul@servalproject.org>
2014-03-23 12:23:42 +1030

    fix compilation errors in board creation.

--- board.c
+++ board.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "2048.h"
 
 int **board_create(int size)
@@ -6,10 +8,10 @@ int **board_create(int size)
   // make sure size of board is sensible
   if (size<1||size>255) return NULL;
 
-  int **board=calloc(size*sizeof(int *));
+  int **board=calloc(size,sizeof(int *));
   if (!board) return NULL;
   for(i=0;i<size;i++) {
-    board[i]=calloc(size*sizeof(int));
+    board[i]=calloc(size,sizeof(int));
     if (!board[i]) return NULL;
   }
   return board;
