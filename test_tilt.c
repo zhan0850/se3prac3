7997247a50860dc391adac8647f70a41f21afa84
gardners <paul@servalproject.org>
2014-03-23 13:36:40 +1030

    add more tests.

--- test_tilt.c
+++ test_tilt.c
@@ -5,7 +5,12 @@ int line_vector_test(int i1,int i2,int i3,int i4,char *msg,
 		int o1,int o2,int o3,int o4, int (*func)(int,int *))
 {
   int list[4]={i1,i2,i3,i4};
-  printf("%s - ",msg); fflush(stdout);
+  if (msg) printf("%s - ",msg); 
+  else {
+    printf("Tilting {%d,%d,%d,%d} left yields {%d,%d,%d,%d} - ",
+	     i1,i2,i3,i4,o1,o2,o3,o4);
+  }
+  fflush(stdout);
   func(4,list);
   if ((list[0]!=o1)||(list[1]!=o2)||(list[2]!=o3)||(list[3]!=o4))
     {
@@ -32,6 +37,10 @@ int test_tilt_left()
   e|=ttl_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
   e|=ttl_vector(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
   e|=ttl_vector(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector(0,0,1,0,"Value in middle shifts to left edge after shift",1,0,0,0);
+  e|=ttl_vector(1,2,4,8,"Distinct values don't combine",1,2,4,8);
+  e|=ttl_vector(1,1,1,1,"Combinations don't cascade",2,2,0,0);
+  e|=ttl_vector(0,0,1,1,NULL,2,0,0,0);
   return e;
 }
 
