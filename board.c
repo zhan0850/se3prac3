ea3c01446ecd8eb8f788666b35a1b8d908b94513
gardners <paul@servalproject.org>
2014-03-23 12:21:52 +1030

    add routine to create board.

---
+++ board.c
@@ -0,0 +1,16 @@
+#include "2048.h"
+
+int **board_create(int size)
+{
+  int i;
+  // make sure size of board is sensible
+  if (size<1||size>255) return NULL;
+
+  int **board=calloc(size*sizeof(int *));
+  if (!board) return NULL;
+  for(i=0;i<size;i++) {
+    board[i]=calloc(size*sizeof(int));
+    if (!board[i]) return NULL;
+  }
+  return board;
+}
