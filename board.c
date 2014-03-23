a2247ce111399ea07921c7e2c5537c9a86d58162
gardners <paul@servalproject.org>
2014-03-23 12:46:23 +1030

    add routine to spawn new tiles on board.

--- board.c
+++ board.c
@@ -33,6 +33,42 @@ int board_display(int size,int **board)
   return 0;
 }
 
+int board_spawn_tile(int size,int **board)
+{
+  // make sure size of board is sensible
+  if (size<1||size>255) return -1;
+
+  // Calculate how many empty squares exist
+  int n=0;
+  int x,y;
+  for(x=0;x<size;x++) 
+    for(y=0;y<size;y++)
+      if (!board[x][y]) n++;
+
+  // no squares free
+  if (n<1) return -1;
+  
+  // pick square to fill
+  n=random()%n;
+
+  // pick value to insert, either 2 or 4
+  int value;
+  if (random()&1) value=2; else value=4;
+
+  // Insert value into board
+  for(x=0;x<size;x++) 
+    for(y=0;y<size;y++)
+      if (!board[x][y]) {
+	if (n) n--; else {
+	  board[x][y]=value;
+	  return 0;
+	}
+      }
+  
+  // Something went wrong
+  return -1;
+}
+
 int board_rotate_90(int size,int **board)
 {
   // make sure size of board is sensible
