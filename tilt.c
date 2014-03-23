c0f99d0e60191c02318950b36b0f5dea9e688315
gardners <paul@servalproject.org>
2014-03-23 12:12:02 +1030

    add code to tilt through empty spaces. also refactor algorithm.

--- tilt.c
+++ tilt.c
@@ -15,9 +15,19 @@ int tilt_line_left(int length,int *line)
   // make sure vector length is sensible
   if (length<1||length>255) return -1;
 
-  // Mark all tiles as not combined
-  int i;
-  int *combinedP[length];
-  for(i=0;i<length;l++) combined[i]=0;
+  // Beginning at the left, try to move the rest of the tiles
+  // left. No combining yet.
+  for(i=0;i<(length-1);i++)
+    tilt_through_empty_space(length,&line[i]);
+  
+  // Combine pairs
+}
 
+int tilt_through_empty_space(int length, int *line)
+{
+  while(!line[0]) {
+    int i;
+    for(i=0;i<(length-1);i++) line[i]=line[i+1];
+  }
+  return 0;
 }
