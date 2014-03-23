b8599208808610033d5712337f1aa78e4b802f9a
gardners <paul@servalproject.org>
2014-03-23 14:46:16 +1030

    add tests for rotating 3x3 board.

--- test_tilt.c
+++ test_tilt.c
@@ -57,6 +57,45 @@ int board_vector_test(int i1,int i2,int i3,int i4,char *msg,
   return 0;
 }
 
+int board3_vector_test(int i1,int i2,int i3,int i4,int i5,
+		       int i6,int i7,int i8,int i9,char *msg,
+		       int o1,int o2,int o3,int o4,int o5,
+		       int o6,int o7,int o8,int o9,
+		       int (*func)(int,int **))
+{
+  int **board=alloca(3*sizeof(int*));
+  board[0]=alloca(sizeof(int)*3);
+  board[1]=alloca(sizeof(int)*3);
+  board[2]=alloca(sizeof(int)*3);
+  board[0][0]=i1; board[1][0]=i2; board[2][0]=i3;
+  board[0][1]=i4; board[1][1]=i5; board[2][1]=i6;
+  board[0][2]=i7; board[1][2]=i8; board[2][2]=i9;
+
+  if (msg) printf("%s - ",msg); 
+  else {
+    printf("Board operation on {{%d,%d,%d},...} yields {{%d,%d,%d}...} - ",
+	     i1,i2,i3,o1,o2,o3);
+  }
+  fflush(stdout);
+  func(3,board);
+  if (
+      (board[0][0]!=o1)||(board[1][0]!=o2)||(board[2][0]!=o3)||
+      (board[0][1]!=o4)||(board[1][1]!=o5)||(board[2][1]!=o6)||
+      (board[0][2]!=o7)||(board[1][2]!=o8)||(board[2][2]!=o9)
+      )
+    {
+      printf("FAILED: {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}} became {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}} instead of"
+	     " {{%d,%d,%d},{%d,%d,%d},{%d,%d,%d}}\n",
+	     i1,i2,i3,i4,i5,i6,i7,i8,i9,
+	     board[0][0],board[1][0],board[2][0],
+	     board[0][1],board[1][1],board[2][1],
+	     board[0][2],board[1][2],board[2][2],
+	     o1,o2,o3,o4,o5,o6,o7,o8,o9);
+      return -1;
+    } 
+  printf("PASSED.\n");
+  return 0;
+}
 
 int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
 	       int o1,int o2,int o3,int o4)
@@ -81,16 +120,22 @@ int test_tilt_left()
 
 int test_rotate_board()
 {
-  board_vector_test(0,0,0,0,"Tilt board left",0,0,0,0,tilt_board_left);
-  board_vector_test(1,0,0,0,"Tilt board left",1,0,0,0,tilt_board_left);
-  board_vector_test(0,1,0,0,"Tilt board left",1,0,0,0,tilt_board_left);
-  board_vector_test(0,1,0,1,"Tilt board left",1,0,1,0,tilt_board_left);
-  board_vector_test(0,1,0,1,"Tilt board up",0,2,0,0,tilt_board_up);
-  board_vector_test(0,2,0,0,"Tilt board up",0,2,0,0,tilt_board_up);
+  board_vector_test(0,0,0,0,"Tilt 2x2 board left",0,0,0,0,tilt_board_left);
+  board_vector_test(1,0,0,0,"Tilt 2x2 board left",1,0,0,0,tilt_board_left);
+  board_vector_test(0,1,0,0,"Tilt 2x2 board left",1,0,0,0,tilt_board_left);
+  board_vector_test(0,1,0,1,"Tilt 2x2 board left",1,0,1,0,tilt_board_left);
+  board_vector_test(0,1,0,1,"Tilt 2x2 board up",0,2,0,0,tilt_board_up);
+  board_vector_test(0,2,0,0,"Tilt 2x2 board up",0,2,0,0,tilt_board_up);
 
-  board_vector_test(1,2,3,4,"Rotate board right (+90)",3,1,4,2,board_rotate_90);
-  board_vector_test(1,2,3,4,"Rotate board 180",4,3,2,1,board_rotate_180);
-  board_vector_test(1,2,3,4,"Rotate board left (+270)",2,4,1,3,board_rotate_270);
+  board_vector_test(1,2,3,4,"Rotate 2x2 board right (+90)",3,1,4,2,board_rotate_90);
+  board_vector_test(1,2,3,4,"Rotate 2x2 board 180",4,3,2,1,board_rotate_180);
+  board_vector_test(1,2,3,4,"Rotate 2x2 board left (+270)",2,4,1,3,board_rotate_270);
+  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 board right (+90)",
+		     7,4,1,8,5,2,9,6,3,board_rotate_90);
+  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 board 180",
+		     9,8,7,6,5,4,3,2,1,board_rotate_180);
+  board3_vector_test(1,2,3,4,5,6,7,8,9,"Rotate 3x3 board left (+270)",
+		     3,6,9,2,5,8,1,4,7,board_rotate_270);
   return 0;
 }
 
