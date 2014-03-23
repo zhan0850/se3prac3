d3ac27f68de0b5dc806ddee16fa121accd5db2a3
gardners <paul@servalproject.org>
2014-03-23 11:40:10 +1030

    add simple main loop that reads keyboard input and decides what to do.

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "2048.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc,char **argv)
+{
+  // Randomise random unmber generator initial state.
+  srandom(time(0));
+  
+  while(1) {
+    printf("Next move? "); fflush(stdout);
+    int action=read_input();
+    switch(action) {
+    case GO_LEFT:
+      break;
+    case GO_RIGHT:
+      break;
+    case GO_UP:
+      break;
+    case GO_DOWN:
+      break;
+    case GO_NOWHERE:
+      break;
+    case INVALID_INPUT:
+    default:
+      printf("Invalid input. Type l, r, u or d to tilt board.\n");
+      break;
+  }
+}
