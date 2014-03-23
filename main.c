cdd2f727ac15831fe5e8e6e7a984a5640f009e1c
gardners <paul@servalproject.org>
2014-03-23 12:46:39 +1030

    spawn tiles at start, and after each move.

--- main.c
+++ main.c
@@ -16,6 +16,10 @@ int main(int argc,char **argv)
     exit(0);
   }
 
+  // Start with two tiles
+  board_spawn_tile(board_size,board);
+  board_spawn_tile(board_size,board);
+
   while(1) {
     board_display(board_size,board);
     printf("Next move? "); fflush(stdout);
@@ -23,15 +27,19 @@ int main(int argc,char **argv)
     switch(action) {
     case GO_LEFT:
       tilt_board_left(board_size,board);
+      board_spawn_tile(board_size,board);
       break;
     case GO_RIGHT:
       tilt_board_right(board_size,board);
+      board_spawn_tile(board_size,board);
       break;
     case GO_UP:
       tilt_board_up(board_size,board);
+      board_spawn_tile(board_size,board);
       break;
     case GO_DOWN:
       tilt_board_down(board_size,board);
+      board_spawn_tile(board_size,board);
       break;
     case GO_NOWHERE:
       break;
