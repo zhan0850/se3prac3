d940a7efd790f3a86a4a87868d878568758708e1
gardners <paul@servalproject.org>
2014-03-23 12:27:02 +1030

    begin implementing board rotation functions.

--- board.c
+++ board.c
@@ -16,3 +16,48 @@ int **board_create(int size)
   }
   return board;
 }
+
+int board_rotate_90(int size,int **board)
+{
+  // make sure size of board is sensible
+  if (size<1||size>255) return NULL;
+  
+  int temp_board[size][size];
+  int x,y;
+
+  // rotate into temp_board
+  for(x=0;x<size;x++) 
+    for(y=0;y<size;y++)
+      temp_board[y][x]=temp_board[x][y];
+  // copy temp board back over board
+  for(x=0;x<size;x++) 
+    for(y=0;y<size;y++)
+      board[x][y]=temp_board[y][x];
+  return 0;
+}
+
+int tilt_board_right(int size,int **board)
+{
+  board_rotate_180(size,board);
+  tilt_board_left(size,board);
+  board_rotate_180(size,board);
+  return 0;
+}
+
+int tilt_board_up(int size,int **board)
+{
+  board_rotate_90(size,board);
+  tilt_board_left(size,board);
+  board_rotate_90(size,board);
+  return 0;
+}
+
+int tilt_board_down(int size,int **board)
+{
+  board_rotate_270(size,board);
+  tilt_board_left(size,board);
+  board_rotate_270(size,board);
+  return 0;
+}
+
+
