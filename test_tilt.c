d0cb769e521d20f454ff8c966325c7579df48a2b
gardners <paul@servalproject.org>
2014-03-23 13:31:43 +1030

    refactor test code to make more reusable. retain error result from all tests.

--- test_tilt.c
+++ test_tilt.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "2048.h"
 
-int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
-	       int o1,int o2,int o3,int o4)
+int line_vector_test(int i1,int i2,int i3,int i4,char *msg,
+		int o1,int o2,int o3,int o4, int (*func)(int,int *))
 {
   int list[4]={i1,i2,i3,i4};
   printf("%s - ",msg); fflush(stdout);
-  tilt_line_left(4,list);
-  int i;
+  func(4,list);
   if ((list[0]!=o1)||(list[1]!=o2)||(list[2]!=o3)||(list[3]!=o4))
     {
       printf("FAILED: {%d,%d,%d,%d} became {%d,%d,%d,%d} instead of"
@@ -21,17 +20,25 @@ int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
   
 }
 
+int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
+	       int o1,int o2,int o3,int o4)
+{
+  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_left);
+}
+
 int test_tilt_left()
 {
-  ttl_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
-  ttl_vector(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
-  ttl_vector(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
+  int e=0;
+  e|=ttl_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
+  e|=ttl_vector(1,0,0,0,"Value on left stays on left after shift",1,0,0,0);
+  e|=ttl_vector(0,0,0,1,"Value on right shifts to left edge after shift",1,0,0,0);
+  return e;
 }
 
 int main(int argc,char **argv)
 {
 
-  test_tilt_left();
+  return test_tilt_left();
 
 }
 
