023b936d8cc622e834f77a19ce6a28d801f7ef56
zhan0850 <zhan0850@flinders.edu.au>
2014-05-22 12:36:22 +0930

    modified keyboard.c - changed keys to tilt board - more convenient

--- keyboard.c
+++ keyboard.c
@@ -10,10 +10,10 @@ int parse_input(char *line)
 	  ||(line[strlen(line)-1]=='\r')))
     line[strlen(line)-1]=0;
 
-  if (!strcasecmp(line,"l")) return GO_LEFT; // left
-  if (!strcasecmp(line,"r")) return GO_RIGHT; // right
-  if (!strcasecmp(line,"u")) return GO_UP; // up
-  if (!strcasecmp(line,"d")) return GO_DOWN; // down
+  if (!strcasecmp(line,"a")) return GO_LEFT; // left
+  if (!strcasecmp(line,"d")) return GO_RIGHT; // right
+  if (!strcasecmp(line,"w")) return GO_UP; // up
+  if (!strcasecmp(line,"s")) return GO_DOWN; // down
   if (line[0]==0) return GO_NOWHERE; // blank input
   return INVALID_INPUT; // invalid input
 }
--- test.c
+++ test.c
@@ -26,7 +26,48 @@ int main(int argc,char **argv)
 
 void test_tilt_left()
 {
+	//initialise input array and output array
+	int size = 4;
+	int **input = board_create(size);
+	int **output = board_create(size);
+	int i, j;
 	
+	//begin testing
+	printf("Tilt Left Tests:\n");
+	
+	//empty board
+	tilt_test("Test Case: empty board is empty after shift ", tilt_board_left, size, input, output);
+	reset(size, input);
+	reset(size, output);
+		
+	//values on left, in middle, and on right
+	for(i = 0; i < size; i++) {
+		input[i][i] = 2;
+		output[0][i] = 2;
+	}
+	tilt_test("Test Case: values on left, in middle, and on right move to left be after shift ", tilt_board_left, size, input, output);
+	reset(size, input);
+	reset(size, output);
+	
+	//combining tiles
+	for(i = 0; i < size; i++) {
+		input[i][0] = output[i][0] = (i+1)*2;
+		for(j = 1; j < size; j++) {
+			input[i][j] = 2;
+			output[0][j] = output[1][j] = 4;
+		}
+	}
+	input[0][2] = input[0][3] = input[1][3] = 4;
+	input[1][2] = 0;
+	output[0][3] = 8;
+	tilt_test("Test Case: same values combine, distinct values don't ", tilt_board_left, size, input, output);
+	reset(size, input);
+	reset(size, output);
+	
+	//done testing, deallocate memory
+	free(input);
+	free(output);
+	printf("\n");
 }
 
 
@@ -47,6 +88,34 @@ void test_tilt_down()
 	
 }
 
+
+void tilt_test(char *msg, int func(int, int**), int size, int **input, int **output)
+{	
+	if(msg)
+		printf("%s\n", msg);
+	fflush(stdout);
+	
+	printf("Input:\n");
+	board_display(size, input);
+	
+	func(size, input);
+	
+	int i, j;
+	for(i = 0; i < size; i++) 
+		for(j = 0; j < size; j++)
+			if(input[i][j] != output[i][j]) {
+				printf("FAILED!\nExpected Output:\n");
+				board_display(size, output);
+				printf("Actual Output:\n");
+				board_display(size, input);
+				return;
+			}
+	
+	printf("PASSED. Actual output matches expected output.\n");
+	board_display(size, output);
+}
+
+
 void test_board_spawn_tile()
 {
 	//initialise input array
@@ -64,6 +133,7 @@ void test_board_spawn_tile()
 		printf("FAILED! Expected output is 2 or 4, but actual output is %d.\n", input[0][0]);
 	
 	//done testing, reset counter, deallocate memory
+	tile_counter(-tile_counter(0));
 	free(input);
 	printf("\n");
 }
@@ -84,7 +154,18 @@ void test_check_lose()
 void test_check_win()
 {
 	
+	
 }
+
+
+void reset(int size, int **board)	//reset an array to all 0s
+{
+	int i, j;
+	for(i = 0; i < size; i++)
+		for(j = 0; j < size; j++) 
+			board[i][j] = 0;
+}
+
 
 /*********************************************************************************
 int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
