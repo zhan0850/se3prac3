c3786ce3eb923ffd5d558e50c49c7254e668f7af
zhan0850 <zhan0850@flinders.edu.au>
2014-05-22 12:11:46 +0930

    modified file 2048, changed functions to be implemented

--- 2048.h
+++ 2048.h
@@ -7,18 +7,22 @@ int read_input();
 #define GO_NOWHERE 0
 #define INVALID_INPUT -1
 
+
+int **board_create(int size);
+int board_display(int size,int **board);
+void board_spawn_tile(int size,int **board);
+int tile_counter(int adder);
 int tilt_board_left(int size,int **board);
 int tilt_board_right(int size,int **board);
 int tilt_board_up(int size,int **board);
 int tilt_board_down(int size,int **board);
-int **board_create(int size);
-int board_display(int size,int **board);
-int board_spawn_tile(int size,int **board);
+void play();
+int check_lose(int size, int **board);
+int check_win(int size, int **board);
+
+
+
+//not used
 int combine_tiles(int length,int *line,int offset);
 int tilt_through_empty_space(int length, int *line);
 int tilt_line_left(int length,int *line);
-
-//additional funcs
-int tilt_line_right(int length,int *line);
-int tilt_line_up(int length,int *line);
-int tilt_line_down(int length,int *line);
