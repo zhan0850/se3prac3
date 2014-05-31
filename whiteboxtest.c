c604fd5e2e8652637b1f3832d02b1461b5b0e03c
zhan0850 <zhan0850@flinders.edu.au>
2014-06-01 01:55:50 +0930

    whiteboxtest.c

--- whiteboxtest.c
+++ whiteboxtest.c
@@ -454,9 +454,28 @@ void whiteboxtest_win() {
 	printf("\n");
 	
 	//TEST CASE
-	printf("TEST CASE: tilt_up: tiles combined, become 2048, won\n");
+	printf("TEST CASE: tilt_up, tiles combined, become 2048, won\n");
+	for(i = 0; i < size; i++)
+		for(j = size-2; j < size; j ++) {
+			input[i][j] = (i+1)*(j+1);
+			tile_counter(1);
+	}
+	input[0][size-2] = input[0][size-1] = 1024;
+	printf("Input:\n");
+	board_display(size, input);
 	
+	output = tilt_board_up(size, input);
+	printf("Output:\n");
+	board_display(size, input);
 	
+	if(output == 2) 
+		printf("Won\nPASSED. Actual output matches expected output.\n");
+	else
+		printf("Not won\nFAILED! Expected output is 0 or 1 but actual output is 2.\n");
+	
+	reset(size, input);
+	tile_counter(-tile_counter(0));
+	printf("\n");
 	
 	//TEST CASE
 	printf("TEST CASE: tilt_down: no tiles combined, no 2048, not won\n");
